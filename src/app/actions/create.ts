130f31166d66d456faae2ece1dc4b487a3f84b63
yongjoo.choi <yongn1994@gmail.com>
2024-06-29 19:14:43 +0900

    refactor: remove unused codes

--- src/app/actions/create.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { cookies } from 'next/headers';
-
-export async function createCookies(token: string, nickname: string) {
-  cookies().set('jwt', token, { secure: true });
-  cookies().set('username', nickname, { secure: true });
-  console.log(cookies().getAll());
-}
