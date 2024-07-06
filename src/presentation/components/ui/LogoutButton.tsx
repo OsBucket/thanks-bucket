fd8fc93b65fd2a9c5cfab5e5849d1380a0f00590
yongjoo.choi <yongn1994@gmail.com>
2024-07-06 22:43:35 +0900

    fix: logout

--- src/presentation/components/ui/LogoutButton.tsx
+++ src/presentation/components/ui/LogoutButton.tsx
@@ -3,21 +3,21 @@ import { deleteCookie } from 'cookies-next';
 import { logout } from '@/services/user';
 import { Button } from './Button';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 function LogoutButton() {
-  const router = useRouter();
-
   const handleLogout = async () => {
-    await logout();
+    logout();
     deleteCookie('jwt');
     await sleep(500);
-    router.push('/');
   };
 
   return (
-    <Button size={'sm'} className="body1Strong" variant={'outline'} onClick={handleLogout}>
-      로그아웃
-    </Button>
+    <Link href={'/'}>
+      <Button size={'sm'} className="body1Strong" variant={'outline'} onClick={handleLogout}>
+        로그아웃
+      </Button>
+    </Link>
   );
 }
 
